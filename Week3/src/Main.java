9589ea40845f11e54122aacf4018554196882bbb
ButterButu <719605178.com>
2023-03-08 19:39:07 +0800

    add w3

--- Week3/src/Main.java
+++ Week3/src/Main.java
@@ -1,5 +1,29 @@
 public class Main {
+    public int threeSumClosest(int[] nums, int target) {
+        int len = nums.length;
+        int min = Integer.MAX_VALUE;
+        int res = 0;
+        for(int i = 0; i < len; i++){
+            for(int j = 0; j < len; j++){
+                if (i == j)
+                    continue;
+                for(int k = 0; k < len; k++){
+                    if (i == k || j == k){
+                        continue;
+                    }
+                    int sum = nums[i] + nums[j] + nums[k];
+                    if(Math.abs(target-sum) < min){
+                        min = Math.abs(target-sum);
+                        res = sum;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Main main = new Main();
+        System.out.println(main.threeSumClosest(new int[]{1,1,1,0},-100));
     }
 }
